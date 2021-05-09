28082685bdecbe2750bc565d0bf75a0594295357
Jimmy Juarez <jimmyaj@uci.edu>
2021-05-08 18:29:43 -0700

    Updated template file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("tESTSETET");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+const prefix = '!';
+
+client.on('ready',() => {
+    console.log("The bot is online!");
+});
+
+client.on('message',(message) => {
+    if(!message.content.startsWith(prefix)||message.author.bot)return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command =  args.shift().toLowerCase();
+
+    if(command === 'zot'){
+        message.channel.send("🤘🤘🤘")
+    }
+});
+
+client.login(process.env.DISCORD_KEY);
