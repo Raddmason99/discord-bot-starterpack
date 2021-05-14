fc3172d86d2837c37e0284cd66122f4d992a265d
Jimmy Juarez <jimmyaj@uci.edu>
2021-05-14 01:09:05 -0700

    Remove code to teach students

--- index.js
+++ index.js
@@ -4,18 +4,8 @@ require('dotenv').config();
 const prefix = '!';
 
 client.on('ready',() => {
-    console.log("The bot is online!");
+    console.log("The bot is online, go check your server!");
 });
 
-client.on('message',(message) => {
-    if(!message.content.startsWith(prefix)||message.author.bot)return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command =  args.shift().toLowerCase();
-
-    if(command === 'zot'){
-        message.channel.send("🤘🤘🤘")
-    }
-});
 
 client.login(process.env.DISCORD_KEY);
